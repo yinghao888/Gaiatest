49fe63f38f83ca25a3c511c40a945494ca9dd77a
yinghao888 <156049291+yinghao888@users.noreply.github.com>
2025-03-05 17:41:45 +0800

    Update gaiachat.sh

--- gaiachat.sh
+++ gaiachat.sh
@@ -36,24 +36,24 @@ set_api_url() {
 
     if [ "$system_type" -eq 0 ]; then
         # VPS
-        API_URL="https://hyper.gaia.domains/v1/chat/completions"
+        API_URL="https://gaias.gaia.domains/v1/chat/completions"
         API_NAME="Hyper"
     elif [ "$system_type" -eq 1 ]; then
         # Laptop
         if [ "$cuda_present" -eq 0 ]; then
-            API_URL="https://soneium.gaia.domains/v1/chat/completions"
+            API_URL="https://gaias.gaia.domains/v1/chat/completions"
             API_NAME="Soneium"
         else
-            API_URL="https://hyper.gaia.domains/v1/chat/completions"
+            API_URL="https://gaias.gaia.domains/v1/chat/completions"
             API_NAME="Hyper"
         fi
     elif [ "$system_type" -eq 2 ]; then
         # Desktop
         if [ "$cuda_present" -eq 0 ]; then
-            API_URL="https://gadao.gaia.domains/v1/chat/completions"
+            API_URL="https://gaias.gaia.domains/v1/chat/completions"
             API_NAME="Gadao"
         else
-            API_URL="https://hyper.gaia.domains/v1/chat/completions"
+            API_URL="https://gaias.gaia.domains/v1/chat/completions"
             API_NAME="Hyper"
         fi
     fi
@@ -80,7 +80,7 @@ fi
 
 # Function to get a random general question based on the API URL
 generate_random_general_question() {
-    if [[ "$API_URL" == "https://hyper.gaia.domains/v1/chat/completions" ]]; then
+    if [[ "$API_URL" == "https://gaias.gaia.domains/v1/chat/completions" ]]; then
 general_questions=(
     "Why is the Renaissance considered a turning point in history?"
     "How did the Industrial Revolution change the world?"
@@ -375,7 +375,7 @@ EOF
     fi
 
     # Set sleep time based on API URL
-    if [[ "$API_URL" == "https://hyper.gaia.domains/v1/chat/completions" ]]; then
+    if [[ "$API_URL" == "https://gaias.gaia.domains/v1/chat/completions" ]]; then
         echo "⏳ Fetching (hyper API)..."
         sleep 1
     elif [[ "$API_URL" == "https://soneium.gaia.domains/v1/chat/completions" ]]; then
